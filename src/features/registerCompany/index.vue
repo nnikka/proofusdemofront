0c67a89b9bf5c45a19f42a12c48cedcd94654031
novaknole <gioskofield@gmail.com>
2018-07-13 21:38:31 +0400

    add testing public/private keys

--- src/features/registerCompany/index.vue
+++ src/features/registerCompany/index.vue
@@ -50,7 +50,8 @@
 </template>
 
 <script>
-
+import keypair from 'keypair'
+import forge from 'node-forge'
 export default {
   data() {
     return {
@@ -73,7 +74,38 @@ export default {
     }
   },
   beforeMount () {
-   
+    
+    var publicKey = `
+-----BEGIN PUBLIC KEY-----
+MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQC864U41PhHDZek0pBdNDgl6lDO
+BGKMJSzmYTtM/ZYfgkf+XbxyMAp63I0YaMnWKGahgngYyhN+D+i0m0Kqcy40dmIH
+/bwy7XUVBs4IMH00upqZ7BgWsvo/qHrAi1p8Go32a9GCM0K0rS4IoQPY594PpEAP
+Wcy5dFspT27fK7n+5QIDAQAB
+-----END PUBLIC KEY-----`;
+    var privateKey = `-----BEGIN RSA PRIVATE KEY-----
+MIICXAIBAAKBgQC864U41PhHDZek0pBdNDgl6lDOBGKMJSzmYTtM/ZYfgkf+Xbxy
+MAp63I0YaMnWKGahgngYyhN+D+i0m0Kqcy40dmIH/bwy7XUVBs4IMH00upqZ7BgW
+svo/qHrAi1p8Go32a9GCM0K0rS4IoQPY594PpEAPWcy5dFspT27fK7n+5QIDAQAB
+AoGAQiI4isKS/1yUv1Au1VHYbf1uuA/055aD90oGVldXXERhK3xo4ZhLsuLOScM1
+FNEjzFo97H+g/zpWfhLz7XNQstabRcdEvLbubI977gWvQ3hETbrGIpMNqgHPJzeC
+Xbtae/hYLMVpuM+6aGrvNgfksftob3/n0nTYHdE5IMfwdqkCQQDpDRxaEGku5KhB
+5pDWMAUPxUD9YRWdCkJSQchRjmtEBLtjTGUByx6tKH6HBj+6YpfgZFVAvP+V9uu9
+lJZovMBjAkEAz4XstFvIAeHdpnr88o6bcSscuynsKUikz4DXeqfJW/18+KDopBUe
+xAUAkXsTr71IUVJxrMlES2eJO3PTftdSFwJBAJlwXWNa/LW4LKO+90teMGbyu1hX
+7FAJ6iKYVxiDSVJuPyYgrTwiDN97Ets5nQnNUiuWQ58tEKYe/ZVDWEg7RfsCQF7n
+PTfCHXaW53miFxmKFBYuBCan5mN+4DgxENn7738NLuC5HYskSXyVWQwu//LHmlHu
+ZSQCqyPcxoqUzqzcJaMCQBH6MxZv2napyhk6NolHQOSDtNOwNs4wFvXn6ux24s8I
+6zqAiYHM4chp6SVW+DGBDh1xD75//jLsP4uHfPoHULg=
+-----END RSA PRIVATE KEY-----`;
+    var encrypt = new JSEncrypt();
+    encrypt.setPublicKey(publicKey);
+    var encrypted = encrypt.encrypt("jordan123");
+
+     var decrypt = new JSEncrypt();
+          decrypt.setPrivateKey(privateKey);
+          var uncrypted = decrypt.decrypt(encrypted);
+    console.log(uncrypted)
+
   },
   computed:{
     getPublicKey(){
